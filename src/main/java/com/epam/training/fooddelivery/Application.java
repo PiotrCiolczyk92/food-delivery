510803a8f4eb5b895f0f246c7a335ba7f24d1df2
Viktor Selmeci <Viktor_Selmeci@epam.com>
2022-01-26 16:25:16 +0100

    added minimal application

---
+++ src/main/java/com/epam/training/fooddelivery/Application.java
@@ -0,0 +1,13 @@
+package com.epam.training.fooddelivery;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Application application = new Application();
+        application.start();
+    }
+
+    private void start() {
+        System.out.println("To be implemented.");
+    }
+}
